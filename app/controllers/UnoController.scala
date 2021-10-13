b70d14b9ed40c1345a0b6ee676adf25d5a1a9375
moritzknauer <poelker.noering@gmx.de>
2021-10-13 16:02:32 +0200

    Add Jar File and Controller

---
+++ app/controllers/UnoController.scala
@@ -0,0 +1,54 @@
+package controllers
+
+import com.google.inject.{Guice, Injector}
+import de.htwg.se.uno.UnoModule
+import de.htwg.se.uno.controller.controllerComponent.ControllerInterface
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class UnoController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+
+  val injector: Injector = Guice.createInjector(new UnoModule)
+  val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
+
+  def index(): Action[AnyContent] = Action {
+    Ok(print())
+  }
+
+  def newGame(): Action[AnyContent] = Action {
+    controller.createGame()
+    Ok(print())
+  }
+
+  def testGame(): Action[AnyContent] = Action {
+    controller.createTestGame()
+    Ok(print())
+  }
+
+  def undo(): Action[AnyContent] = Action {
+    controller.undo()
+    Ok(print())
+  }
+
+  def redo(): Action[AnyContent] = Action {
+    controller.redo()
+    Ok(print())
+  }
+
+  def save(): Action[AnyContent] = Action {
+    controller.save()
+    Ok(print())
+  }
+
+  def load(): Action[AnyContent] = Action {
+    controller.load()
+    Ok(print())
+  }
+
+  def print(): String = {
+    s"${controller.gameToString}"
+  }
+
+}
