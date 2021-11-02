4effe416f662a6c105b6c66a5684aa85d02806de
Florian Lanz <fl631lan@htwg-konstanz.de>
2021-11-02 19:06:29 +0100

    change javascript draggable and add active player glow

--- public/javascripts/uno.js
+++ public/javascripts/uno.js
@@ -56,24 +56,52 @@ class CardDeck {
     }
 }
 
+let onDropzone = false;
+
 $(document).ready(function() {
-    // let cardDeck = new CardDeck();
-    // for (let i = 0; i < cardDeck.cardDeck.length; i++) {
-    //     console.log(cardDeck.cardDeck[i].toString());
-    // }
+    nextStep();
 });
 
 const droppable = new Draggable.Droppable(document.querySelectorAll('.container-dropzone'), {
     draggable: '.item-draggable',
-    dropzone: '.dropzone'
+    dropzone: '.dropzone',
+    classes: {
+        'source:dragging': 'hide-card',
+        'mirror': 'mirror-card',
+    },
 });
 
 droppable.on('droppable:dropped', (event) => {
+    onDropzone = true;
     const card = document.getElementById('open-card-stack');
     card.classList.add('card-deck-dropzone');
 });
 
+droppable.on('droppable:returned', () => {
+    onDropzone = false;
+    const card = document.getElementById('open-card-stack');
+    card.classList.remove('card-deck-dropzone');
+});
+
 droppable.on('droppable:stop', (event) => {
     const card = event.dragEvent.originalSource.id;
-    window.location = '/set/' + card;
+    if (onDropzone) {
+        nextStep();
+        window.location = '/set/' + card;
+    }
 });
+
+async function nextStep() {
+    // $('#myToast').toast('show');
+    await Sleep(1000);
+    const gameText = document.getElementById('game-text');
+    const state = gameText.innerText;
+    if (state !== 'Du bist am Zug' && state !== 'Wähle eine Farbe' && state !== 'Glückwunsch, du hast gewonnen!' && state !== 'Du hast leider verloren') {
+        window.location = '/dostep';
+    }
+
+}
+
+function Sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
