592d143a449b90a0d2ac1ba21a9454df86101937
Florian Lanz <fl631lan@htwg-konstanz.de>
2021-11-16 11:28:05 +0100

    add websocket to server and client

--- app/controllers/UnoController.scala
+++ app/controllers/UnoController.scala
@@ -1,17 +1,21 @@
 package controllers
 
+import akka.actor._
+import akka.stream.Materializer
 import com.google.inject.{Guice, Injector}
 import de.htwg.se.uno.UnoModule
-import de.htwg.se.uno.controller.controllerComponent.{ControllerInterface, GameNotChanged}
+import de.htwg.se.uno.controller.controllerComponent.{ChooseColor, ControllerInterface, GameChanged, GameEnded, GameNotChanged, GameSizeChanged}
+import play.api.Play.materializer
 import play.api.libs.json._
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
-import play.api.routing.{JavaScriptReverseRoute, JavaScriptReverseRouter}
-import play.twirl.api.{Html, MimeTypes}
+import play.api.libs.streams.ActorFlow
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, WebSocket}
+import play.twirl.api.Html
 
 import javax.inject.{Inject, Singleton}
+import scala.swing.Reactor
 
 @Singleton
-class UnoController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+class UnoController @Inject() (val controllerComponents: ControllerComponents) (implicit system: ActorSystem, mat: Materializer) extends BaseController {
 
   val injector: Injector = Guice.createInjector(new UnoModule)
   val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
@@ -98,31 +102,69 @@ class UnoController @Inject() (val controllerComponents: ControllerComponents) e
     Ok(views.html.index())
   }
 
-  def gameToJson(): Action[AnyContent] = Action {
-    Ok(
-      Json.prettyPrint(
-        Json.obj(
-          "game" -> Json.obj(
-            "numOfPlayers" -> JsNumber(controller.getNumOfPlayers),
-            "nextTurn" -> JsBoolean(controller.nextTurn()),
-            "nextEnemy" -> JsNumber(controller.nextEnemy()),
-            "gameText" -> JsString(controller.controllerEvent("idle")),
-            "specialCard" -> JsString(controller.getHs2),
-            "enemy1Cards" -> JsNumber(controller.getLength(0)),
-            "enemy2Cards" -> JsNumber(controller.getLength(1)),
-            "enemy3Cards" -> JsNumber(controller.getLength(2)),
-            "openCardStack" -> JsString(controller.getCardText(3, 1)),
-            "playerCards" -> JsArray(
-              for {
-                cardNumber <- 0 until controller.getLength(4)
-              } yield {
-                JsString(controller.getCardText(4, cardNumber))
-              }
-            ),
-          )
+  def gameToJsonAction(): Action[AnyContent] = Action {
+    Ok(gameToJson())
+  }
+
+  def gameToJson(): String = {
+    Json.prettyPrint(
+      Json.obj(
+        "game" -> Json.obj(
+          "numOfPlayers" -> JsNumber(controller.getNumOfPlayers),
+          "nextTurn" -> JsBoolean(controller.nextTurn()),
+          "nextEnemy" -> JsNumber(controller.nextEnemy()),
+          "gameText" -> JsString(controller.controllerEvent("idle")),
+          "specialCard" -> JsString(controller.getHs2),
+          "enemy1Cards" -> JsNumber(controller.getLength(0)),
+          "enemy2Cards" -> JsNumber(controller.getLength(1)),
+          "enemy3Cards" -> JsNumber(controller.getLength(2)),
+          "openCardStack" -> JsString(controller.getCardText(3, 1)),
+          "playerCards" -> JsArray(
+            for {
+              cardNumber <- 0 until controller.getLength(4)
+            } yield {
+              JsString(controller.getCardText(4, cardNumber))
+            }
+          ),
         )
       )
     )
   }
 
+
+  def socket: WebSocket = WebSocket.accept[String, String] { request =>
+    ActorFlow.actorRef { out =>
+      println("Connect received")
+      UnoWebSocketActorFactory.create(out)
+    }
+  }
+
+  object UnoWebSocketActorFactory {
+    def create(out: ActorRef): Props = {
+      Props(new UnoWebSocketActor(out))
+    }
+  }
+
+  class UnoWebSocketActor(out: ActorRef) extends Actor with Reactor {
+    listenTo(controller)
+
+    def receive: Receive = {
+      case msg: String =>
+        out ! gameToJson()
+        println("Sent Json to Client"+ msg)
+    }
+
+    reactions += {
+      case event: GameSizeChanged => sendJsonToClient
+      case event: GameChanged => sendJsonToClient
+      case event: GameNotChanged => sendJsonToClient
+      case event: ChooseColor => sendJsonToClient
+      case event: GameEnded => sendJsonToClient
+    }
+
+    def sendJsonToClient(): Unit = {
+      println("Received event from Controller")
+      out ! gameToJson()
+    }
+  }
 }
--- public/javascripts/uno.js
+++ public/javascripts/uno.js
@@ -341,10 +341,37 @@ async function nextStep() {
     }
 }
 
+function connectWebSocket() {
+    let websocket = new WebSocket("ws://localhost:9000/websocket");
+    websocket.setTimeout
+
+    websocket.onopen = function(event) {
+        console.log("Connected to Websocket");
+    }
+
+    websocket.onclose = function () {
+        console.log('Connection with Websocket Closed!');
+    };
+
+    websocket.onerror = function (error) {
+        console.log('Error in Websocket Occured: ' + error);
+    };
+
+    websocket.onmessage = function (e) {
+        if (typeof e.data === "string") {
+            let json = JSON.parse(e.data);
+            console.log(json);
+        }
+
+    };
+}
+
+
 /**
  * On Load
  *
  */
 $(document).ready(function() {
     loadJson();
+    connectWebSocket();
 });
